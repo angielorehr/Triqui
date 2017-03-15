bfd8b8dc4fd8f00a41c31055f131597ecc1259a2
Estudiantes <Estudiantes@Labing50613.udistrital.edu.co>
2017-03-15 15:26:15 -0500

    Copia Explicacion profesor

--- Triqui/src/juego/Tablero.java
+++ Triqui/src/juego/Tablero.java
@@ -11,29 +11,50 @@ package juego;
  */
 public class Tablero {
 
-    int jugador;
-    char ficha;
-    
-    void calculartriqui(){  
+    char[][] fichas = new char[3][3];
+
+    public Tablero() {
+        
+        limpiarTablero();
     }
-    public String Asignacion(){
-        //En turno vamos a definir si es una X o una Y
-    //0 es X
-    //1 es Y
-    int turno=0;
-       
-    if(turno==0){
-            turno=1;
-            return("X");
-        }else{
-            turno=0;
-             return("O");
+    
+    
+
+    int calcularTriqui() {
+
+        //Filas
+        for (int i = 0; i < 3; i++) {
+            if (fichas[i][0]!='a'&& fichas[i][0] == fichas[i][1] && fichas[i][0] == fichas[i][2]) {
+                return 1;
+            }
+        }
+
+        //Columnas
+        for (int i = 0; i < 3; i++) {
+            if (fichas[0][i]!='a'&&fichas[0][i] == fichas[1][i] && fichas[0][i] == fichas[2][i]) {
+                return 1;
+            }
+        }
+
+        //Diagonales
+        if (fichas[0][0]!='a'&&fichas[0][0] == fichas[1][1] && fichas[0][0] == fichas[2][2]) {
+            return 1;
+        }
+
+        if (fichas[1][1]!='a'&&fichas[2][0] == fichas[1][1] && fichas[2][0] == fichas[0][2]) {
+            return 1;
+        }
+
+        return 0;
     }
+
+    void limpiarTablero() {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                fichas[i][j]='a';
+            }
+        }
+    }
+
 }
-}
-
-    
-
-    
-    
-
